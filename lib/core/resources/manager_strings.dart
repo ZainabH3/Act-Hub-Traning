70f70c0eebdb451f01e4c4f19ab9b211ab050e99
ZainabH3 <devzhsoftware@gmail.com>
2023-05-18 12:57:06 +0300

    create file manager-strings.dart

---
+++ lib/core/resources/manager_strings.dart
@@ -0,0 +1,149 @@
+class ManagerStrings {
+  static const String noRouteFound = 'No Route Found';
+  static const String actHub = 'Act-Hub';
+  static const String logoutFailed = 'Logout Failed';
+  static const String outBoardingView = 'Out Boarding View';
+  static const String outBoardingTitle1 = 'Find your cozy working space';
+  static const String outBoardingTitle2 = 'Find your cozy working space';
+  static const String outBoardingTitle3 = 'Find your cozy working space';
+  static const String outBoardingSubTitle1 =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.';
+  static const String outBoardingSubTitle2 =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.';
+  static const String outBoardingSubTitle3 =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.';
+  static const String login = 'Login';
+  static const String signUp = 'Sign Up';
+  static const String email = "Email";
+  static const String status = "status";
+  static const String location = "Location";
+  static const String profile = "Profile";
+  static const String edit = "Edit";
+  static const String setting = "Setting";
+  static const String rates = "Rate";
+  static const String share = "Share App";
+  static const String document = "Instructions for use";
+  static const String logout = "log Out";
+  static const String error = "Error";
+  static const String shouldAgreeTerms = "You Should Agree The Terms";
+  static const String password = "Password";
+  static const String fullName = "Full Name";
+  static const String userName = "User Name";
+  static const String dayOfBirth = "Day of Birth";
+  static const String saveEdit = "Save Edit";
+  static const String phoneNum = "Phone Num";
+  static const String notifications = "Notifications";
+  static const String general = "General";
+  static const String bookAgain = "Book Again";
+  static const String darkMode = "Dark mode";
+  static const String language = "Language";
+  static const String payment = "Payment Method";
+  static const String account = "Account";
+  static const String editProfile = "Edit Profile";
+  static const String changePassword = "Change Password";
+  static const String myAppointment = "My Appointment";
+  static const String upcoming = "Upcoming";
+  static const String completed = "Completed";
+  static const String cancelled = "Cancelled";
+  static const String sharedWorkspace = "Shared workspace";
+  static const String cancel = "Cancel";
+  static const String reservationView = "Reservation view";
+  static const String activation = "activation";
+  static const String popularCourses = "Popular Courses";
+  static const String search = "Search";
+  static const String hintSearch = "Try Search for Courses, or Categories";
+  static const String confirmPass = "Confirm Password";
+  static const String skip = 'Skip';
+  static const String saved = 'Saved';
+  static const String uiuxDesign = 'UI / UX Design';
+  static const String hourTraining = '45 hour training';
+  static const String lecturerName = 'Ramadan Ghattas';
+  static const String description = 'Description';
+  static const String courseDescription =
+      'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy\neirmod tempor invidunt ut labore et\ndolore magna aliquyam erat, sed diam\nvoluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita\nkasd gubergren, no sea takimata\nsanctus est Lorem ipsum dolor sit ';
+  static const String empty = '';
+  static const String categories = 'Categories';
+  static const String seeAll = 'See All';
+  static const String viewAll = 'View All';
+  static const String bookNow = 'Book now';
+  static const String rate = '4,8/5';
+  static const String review = '(102 Review)';
+  static const String hours = '45 Hour';
+  static const String descriptionCourses =
+      'Basic User Interface \nDesign for Beginner';
+  static const String office = 'Office';
+  static const String design = 'DESIGN';
+  static const String meetingRoom = 'Meeting Room';
+  static const String sharedDesk = 'Shared Desk';
+  static const String recordingStudio = 'recording studio';
+  static const String playStation = 'Play Station';
+  static const String board = 'Board';
+  static const String rememberMe = 'Remember me';
+  static const String forgetPassword = 'Forget Password ?';
+  static const String startButton = "Let's get started";
+  static const String verificationCode = "Verification Code";
+  static const String subTitleVerification = "Enter the code please";
+  static const String continueButton = "Continue";
+  static const String footerLogin = 'Don’t have any account ?';
+  static const String footerSignUp = 'Do you Have Account ?';
+  static const String confirm = 'Confirm';
+  static const String titleBanner = "UI / UX Design";
+  static const String checkBoxName =
+      "I agree to our Terms of service and Privacy policy";
+
+  static const String invalidEmptyEmail = 'Please Enter Email';
+  static const String thanks = 'Thanks';
+  static const String invalidEmail = 'Please Enter A Valid Email';
+  static const String forgetSubTitle =
+      'To reset your password, enter \n your e-mail';
+
+  static const String invalidPasswordLength =
+      'Passwords must have at least 8 characters';
+  static const String invalidPasswordUpper =
+      'Passwords must have at least one uppercase character';
+  static const String invalidPasswordLower =
+      'Passwords must have at least one lowercase character';
+  static const String invalidPasswordDigit =
+      'Passwords must have at least one number';
+
+  static const String invalidConditionAccept =
+      'Please Satisfy The Conditions & Terms';
+
+  static const String passwordNotMatch = 'The Password Not Match';
+  static const String invalidEmptyPhoneNumber = 'Enter Phone Number';
+  static const String invalidEmptyCode = "This field is required";
+  static const String invalidPhoneNumber = 'Enter A Valid Phone Number';
+  static const String notVerifiedEmail = 'Your email address is not verified..';
+
+  static const String invalidEmptyFullName = 'Enter A Full Name';
+  static const String invalidFullName = 'Enter A Valid Full Name';
+  static const String sorryFailed = 'Sorry Failed';
+  static const String loading = 'Loading...';
+  static const String ok = 'ok';
+  static const String registerFailed = 'Register Failed';
+  static const String registerSuccess = 'Register Success';
+  static const String loginFailed = 'Login Failed';
+  static const String loginSuccess = 'Login Success';
+  static const String verificationSuccess =
+      'The code has been verified, login now';
+  static const String tokenKey = 'token';
+  static const String message = 'message';
+  static const String SUCCESS = 'The request was successful.';
+  static const String NO_CONTENT =
+      'The request has no content. Please try again later.';
+  static const String BAD_REQUEST = "Sorry Bad Request, Try Again Later";
+  static const String FORBIDDEN =
+      "Sorry The Request Forbidden, Try Again Later";
+  static const String UNAUTHORISED = "Sorry You Should SignIn, Try Again Later";
+  static const String NOT_FOUND = "Sorry Not Found, Try Again Later";
+  static const String INTERNAL_SERVER_ERROR =
+      "Internal Server Error, Try Again Later";
+  static const String CONNECT_TIMEOUT = "Connection Time Out, Try Again Later ";
+  static const String CANCEL = "The Request Canceled, Try Again Later";
+  static const String RECIEVE_TIMEOUT = "Recieve Time Out, Try Again Later";
+  static const String SEND_TIMEOUT = "Sending Time Out, Try Again Later";
+  static const String CACHE_ERROR = "Catching Time Error, Try Again Later";
+  static const String NO_INTERNT_CONNECTION =
+      "No Internet Connection, Try Again Later";
+  static const String UNKNOWN = "UnKnown, Try Again Later";
+}
