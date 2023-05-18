04a5c6c27ac7fb489010bc87aa75de6d0a24c999
ZainabH3 <devzhsoftware@gmail.com>
2023-05-18 13:40:07 +0300

    add manager size util

---
+++ lib/core/resources/manager_sizes.dart
@@ -0,0 +1,47 @@
+import '../util/size-util.dart';
+import '../util/size_util.dart';
+
+class ManagerRadius {
+  static double r2 = SizeUtil.setRadius(2.0);
+  static double r4 = SizeUtil.setRadius(4.0);
+  static double r6 = SizeUtil.setRadius(6.0);
+  static double r8 = SizeUtil.setRadius(8.0);
+  static double r10 = SizeUtil.setRadius(10.0);
+  static double r12 = SizeUtil.setRadius(12.0);
+}
+
+class ManagerWidth {
+  static double w1 = SizeUtil.setWidth(1.0);
+  static double w2 = SizeUtil.setWidth(2.0);
+  static double w4 = SizeUtil.setWidth(4.0);
+  static double w6 = SizeUtil.setWidth(6.0);
+  static double w8 = SizeUtil.setWidth(8.0);
+  static double w10 = SizeUtil.setWidth(10.0);
+  static double w12 = SizeUtil.setWidth(12.0);
+}
+
+class ManagerHeight {
+  static double h2 = SizeUtil.setHeight(2.0);
+  static double h4 = SizeUtil.setHeight(4.0);
+  static double h6 = SizeUtil.setHeight(6.0);
+  static double h8 = SizeUtil.setHeight(8.0);
+  static double h10 = SizeUtil.setHeight(10.0);
+  static double h12 = SizeUtil.setHeight(12.0);
+}
+
+class ManagerSize {
+  static double s0_5 = SizeUtil.setRadius(0.5);
+  static double s1_5 = SizeUtil.setRadius(1.5);
+  static double s2 = SizeUtil.setRadius(2.0);
+  static double s4 = SizeUtil.setRadius(4.0);
+  static double s6 = SizeUtil.setRadius(6.0);
+  static double s8 = SizeUtil.setRadius(8.0);
+  static double s10 = SizeUtil.setRadius(10.0);
+  static double s12 = SizeUtil.setRadius(12.0);
+}
+
+class ManagerOpacity {
+  static double Op_0_2 = 0.2;
+  static double Op_0_4 = 0.4;
+  static double Op_0_6 = 0.6;
+}
