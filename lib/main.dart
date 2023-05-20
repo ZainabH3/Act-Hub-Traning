0808ce9d0e19fafc72b65c792255a8ab0e551da7
ZainabH3 <devzhsoftware@gmail.com>
2023-05-21 00:46:36 +0300

    add Get Material App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
-
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      // onGenerateRoute: ,
+      // initialRoute: ,
     );
   }
 }
