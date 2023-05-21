1845aa089a00a2b4583cfcf1e056e75c81ad8811
ZainabH3 <devzhsoftware@gmail.com>
2023-05-21 14:03:00 +0300

    edit constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
-
+  static const String baseUrl = 'http://example.com';
+  static const double deviceWidth = 375;
+  static const double deviceHeight = 812;
+  static const int  splashDuration = 3;
 }
