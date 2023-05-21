fcdc115b99cad73f12031722bb6045fb2627d3a4
ZainabH3 <devzhsoftware@gmail.com>
2023-05-21 14:03:45 +0300

    edite main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:act_hub_training/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+
+import 'config/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      // onGenerateRoute: ,
-      // initialRoute: ,
+    return ScreenUtilInit(
+      splitScreenMode: true,
+      minTextAdapt: true,
+      designSize: const Size(
+        Constants.deviceWidth,
+        Constants.deviceHeight,
+      ),
+      builder: (context, child) {
+        return const GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.splashView,
+        );
+      },
     );
   }
 }
