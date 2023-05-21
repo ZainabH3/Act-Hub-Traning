9489feec38c48cba99043ace89fe0fb476e4ff0a
ZainabH3 <devzhsoftware@gmail.com>
2023-05-21 14:06:21 +0300

    edit splash-view.dart

---
+++ lib/fetaures/splash/presentaion/view/splash-view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../core/resources/manager-assets.dart';
+import '../../../../core/resources/manager_sizes.dart';
+import '../../../../core/widget/scaffold-width-background.dart';
+
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return scaffoldWithBackGroundImage(
+      child: Center(
+        child: SvgPicture.asset(
+          ManagerAssets.logo,
+          width: ManagerWidth.w166,
+        ),
+      ),
+    );
+  }
+}
