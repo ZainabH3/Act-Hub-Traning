9e2f6c6bdda74145bec561b439ddf6e064628de8
ZainabH3 <devzhsoftware@gmail.com>
2023-05-21 14:04:38 +0300

    create out boarding view

---
+++ lib/fetaures/out-boarding-view/out-boarding-view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class OutBoardingView extends StatelessWidget {
+  const OutBoardingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
