d81353642f73cd28aa8e9713c4ce4a1ff3781b55
ZainabH3 <devzhsoftware@gmail.com>
2023-05-22 22:56:54 +0300

    add theme-service.dart

---
+++ lib/core/service/theme-service.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import '../theme/manager_dark_theme.dart';
+import '../theme/manager_light_theme.dart';
+
+class ThemeService {
+  ThemeMode getThemeMode() => ThemeMode.light;
+
+  ThemeData getThemeData() => managerLightThemeData();
+}
