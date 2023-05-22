1628b23633a070f80a75bd4345e8af1257d0a35b
ZainabH3 <devzhsoftware@gmail.com>
2023-05-22 22:52:26 +0300

    edit constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -3,4 +3,5 @@ class Constants {
   static const double deviceWidth = 375;
   static const double deviceHeight = 812;
   static const int  splashDuration = 3;
+  static const double elevationAppBar = 0;
 }
