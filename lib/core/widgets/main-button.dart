a1dfe6e75ffa9de0f2e3acb1b30ebb069f1bbbe2
ZainabH3 <devzhsoftware@gmail.com>
2023-05-23 14:26:10 +0300

    create main button

---
+++ lib/core/widgets/main-button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../config/constants.dart';
+import '../resources/manager-colors.dart';
+import '../resources/manager_font.dart';
+import '../resources/manager_sizes.dart';
+import '../resources/manager_strings.dart';
+import '../resources/manager_styles.dart';
+
+Widget mainButton({
+  required Widget child,
+  ShapeBorder? shapeBorder,
+  Color? color,
+  double? minWidth,
+  double? height,
+  double? elevation,
+}) {
+  return MaterialButton(
+    onPressed: () {},
+    shape: shapeBorder ??
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            ManagerRadius.r12,
+          ),
+        ),
+    color: color ?? ManagerColors.transparent,
+    minWidth: minWidth ?? ManagerWidth.w16,
+    height: height ?? ManagerHeight.h16,
+    elevation: elevation ?? Constants.elevationButton,
+    child: child,
+  );
+}
