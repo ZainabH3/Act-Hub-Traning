d905b1d2b8dcee8addb250cacdce030829898fa9
ZainabH3 <devzhsoftware@gmail.com>
2023-05-25 14:05:44 +0300

    edite

--- lib/config/dependency-injection.dart
+++ lib/config/dependency-injection.dart
@@ -1,11 +1,27 @@
 
+import 'package:act_hub_training/core/storage/local/app-settings-shared-preferences.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../features/out_boarding/presentation/controller/out_boarding_controller.dart';
 import '../features/splash/presentation/controller/splash_controller.dart';
 
 final instance = GetIt.instance;
+
+initModule() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences sharedPreferences =
+  await SharedPreferences.getInstance();
+
+  instance.registerLazySingleton<SharedPreferences>(
+        () => sharedPreferences,
+  );
+
+  instance.registerLazySingleton<AppSettingSharedPreferences>(
+          () => AppSettingSharedPreferences(instance()));
+}
 
 initSplash() {
   Get.put<SplashController>(SplashController());
