3815ee9d4f7773140a331d75e516647f394ab7d9
ZainabH3 <devzhsoftware@gmail.com>
2023-05-28 15:59:29 +0300

    create doi factory

---
+++ lib/core/network/doi-factory.dart
@@ -0,0 +1,46 @@
+import 'package:act_hub_training/core/storage/local/app-settings-shared-preferences.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import '../../config/constants.dart';
+import '../../config/dependency-injection.dart';
+
+class DioFactory {
+  final AppSettingSharedPreferences _appSettingsPreferences =
+  instance<AppSettingSharedPreferences>();
+
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+    Map<String, String> headers =
+    {'authorization': 'Bearer ${_appSettingsPreferences.getToken()}',
+    };
+    dio.options = BaseOptions(
+      baseUrl: Constants.baseUrl,
+      headers: headers,
+
+      receiveTimeout: const Duration(
+          seconds: ApiConstants.receiveTimeOutDuration),
+      sendTimeout: const Duration(seconds: ApiConstants.sendTimeOutDuration),
+    );
+    InterceptorsWrapper interceptorsWrapper = InterceptorsWrapper(onRequest:
+        (RequestOptions options, RequestInterceptorHandler handler) async {
+      String token = _appSettingsPreferences.getToken();
+      options.headers['Authorization'] = 'Bearer $token';
+      return handler.next(options);
+    });
+    dio.interceptors.add(interceptorsWrapper);
+    if (!kReleaseMode) {
+      dio.interceptors.add(
+        PrettyDioLogger(
+          requestBody: true,
+          responseHeader: true,
+          responseBody: true,
+          requestHeader: true,
+          request: true,
+
+        ),
+      );
+    }
+    return dio;
+  }}
