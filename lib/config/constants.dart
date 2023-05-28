d06b280cf87c34a498d6d66e5f8a573b1a0ed2af
ZainabH3 <devzhsoftware@gmail.com>
2023-05-28 15:59:46 +0300

    add constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,4 @@
 class Constants {
-  static const String baseUrll = 'http://example.com';
   static const String baseUrl = 'http://act-hub.actit.ps';
   static const double deviceWidth = 375;
   static const double deviceHeight = 812;
@@ -8,9 +7,18 @@ class Constants {
   static const double elevationAppBar = 0;
   static const double elevationButton = 0;
   static const int sliderItems = 3;
+  static const String tokenKey = 'tokenKey';
+  static const String outBoardingViewedKey = 'out_boarding_viewed';
 }
-
 class ConstantsPrefsKeys {
   static const String outBoardingViewedKey = 'out_boarding_viewed';
+  static const String token = 'token';
+}
 
+
+class ApiConstants{
+  static const String baseUrl = 'http://act-hub.actit.ps';
+  static const String authorization = 'authorization';
+  static const int sendTimeOutDuration = 120;
+  static const int receiveTimeOutDuration = 120;
 }
