342bb440eea1b5f4d1879ffd6c55301d5f3e9dd2
ZainabH3 <devzhsoftware@gmail.com>
2023-05-28 14:10:27 +0300

    edite

--- lib/core/network/app-api.dart
+++ lib/core/network/app-api.dart
@@ -1,11 +1,17 @@
+
+import 'package:act_hub_training/config/constants.dart';
+import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
+part 'app-api.g.dart';
 
-import '../../config/constants.dart';
-
-part 'app_api.g.dart';
-
-
-@RestApi(baseUrl:Constants.baseUrl )
-abstract class AppApi {
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppApi{
+  factory AppApi(
+      Dio dio, {
+        String baseUrl,
+      }) = _AppApi;
 
 }
+
+
+
