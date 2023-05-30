420a4161da3bcb0113bc1210c238134145b202f9
ZainabH3 <devzhsoftware@gmail.com>
2023-05-30 15:50:45 +0300

    create login-mapper.dart

--- lib/features/auth/data/mapper/login-mapper.dart
+++ lib/features/auth/data/mapper/login-mapper.dart
@@ -1,6 +1,7 @@
 import 'package:act_hub_training/core/extensions/extensions.dart';
 
-import '../domain/model/login-model.dart';
+import '../../domain/model/login-model.dart';
+
 import '../response/login-response.dart';
 
 extension LoginMapper on LoginResponse{
