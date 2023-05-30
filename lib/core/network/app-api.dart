06da7ee84136ce32a0ec7b68b9a574cbc66539e8
ZainabH3 <devzhsoftware@gmail.com>
2023-05-30 14:02:26 +0300

    edite

--- lib/core/network/app-api.dart
+++ lib/core/network/app-api.dart
@@ -1,7 +1,11 @@
 
-import 'package:act_hub_training/config/constants.dart';
+
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
+
+import '../../config/constants.dart';
+import '../../config/request-constants.dart';
+import '../../features/auth/data/response/login-response.dart';
 part 'app-api.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
@@ -11,6 +15,10 @@ abstract class AppApi{
         String baseUrl,
       }) = _AppApi;
 
+
+  @POST(RequestConstants.login)
+  Future<LoginResponse> login(
+      @Field(ApiConstants.email) email, @Field(ApiConstants.password) password);
 }
 
 
