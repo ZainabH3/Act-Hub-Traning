685141c7b3f61dcf7eff0bce4633d8cf748171ce
ZainabH3 <devzhsoftware@gmail.com>
2023-05-30 14:02:39 +0300

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -21,4 +21,12 @@ class ApiConstants{
   static const String authorization = 'authorization';
   static const int sendTimeOutDuration = 120;
   static const int receiveTimeOutDuration = 120;
+
+  static const String email = 'email';
+  static const String password = 'password';
+  static const String message = 'message';
+  static const String error = 'error';
+  static const String errors = 'errors';
+  static const String badRequest = 'Bad Request';
 }
+
