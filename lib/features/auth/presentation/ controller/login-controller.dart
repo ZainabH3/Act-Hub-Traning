23ab62c6bd859dd7cd066fc83394f281a44fe582
ZainabH3 <devzhsoftware@gmail.com>
2023-06-04 14:01:48 +0300

    edite login-controller

--- lib/features/auth/presentation/ controller/login-controller.dart
+++ lib/features/auth/presentation/ controller/login-controller.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-
+import 'package:flutter_state_render_dialog/flutter_state_render_dialog.dart';
+import 'package:get/get.dart';
 import '../../../../config/dependency-injection.dart';
+import '../../../../core/resources/manager-colors.dart';
+import '../../../../core/resources/manager_font.dart';
+import '../../../../core/resources/manager_sizes.dart';
+import '../../../../core/resources/manager_strings.dart';
+import '../../../../core/resources/manager_styles.dart';
+import '../../../../core/widgets/main-button.dart';
 import '../../domain/use-case/login-use-case.dart';
 
 class LoginController extends GetxController {
@@ -10,13 +16,72 @@ class LoginController extends GetxController {
   late final LoginUseCase _loginUseCase = instance<LoginUseCase>();
   var formKey = GlobalKey<FormState>();
 
-  Future<void> login() async {
+  Future<void> login(BuildContext context) async {
+    dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpLoadingState,
+        message: ManagerStrings.loading,
+        title: '',
+        retryAction: () {});
     (await _loginUseCase.execute(
       LoginUseCaseInput(email: email.text, password: password.text),
-    )).fold((l) {
-      print('failed');
+    ))
+        .fold((l) {
+      Get.back();
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpErrorState,
+        message: l.message,
+        title: '',
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w65,
+          ),
+          child: mainButton(
+            child: Text(
+              ManagerStrings.ok,
+              style: getMediumTextStyle(
+                fontSize: ManagerFontSize.s16,
+                color: ManagerColors.white,
+              ),
+            ),
+            onPressed: () {
+              Get.back();
+            },
+            color: ManagerColors.primaryColor,
+            height: ManagerHeight.h40,
+          ),
+        ),
+        retryAction: () {},
+      );
     }, (r) {
-      print('success');
+      Get.back();
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpSuccessState,
+        message: ManagerStrings.thanks,
+        title: '',
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w65,
+          ),
+          child: mainButton(
+            child: Text(
+              ManagerStrings.ok,
+              style: getMediumTextStyle(
+                fontSize: ManagerFontSize.s16,
+                color: ManagerColors.white,
+              ),
+            ),
+            onPressed: () {
+              Get.back();
+            },
+            color: ManagerColors.primaryColor,
+            height: ManagerHeight.h40,
+          ),
+        ),
+        retryAction: () {},
+      );
     });
   }
 }
