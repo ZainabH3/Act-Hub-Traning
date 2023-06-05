ad21b81d655618c112f30f44d1844167f371d55e
ZainabH3 <devzhsoftware@gmail.com>
2023-06-05 22:21:05 +0300

    edite app-settings-shared-preferences.dart

--- lib/core/storage/local/app-settings-shared-preferences.dart
+++ lib/core/storage/local/app-settings-shared-preferences.dart
@@ -3,11 +3,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../config/constants.dart';
 
-class AppSettingSharedPreferences{
 
 
-   final SharedPreferences _sharedPreferences;
-  AppSettingSharedPreferences (this._sharedPreferences);
+class AppSettingsSharedPreferences {
+  final SharedPreferences _sharedPreferences;
+
+  AppSettingsSharedPreferences(this._sharedPreferences);
 
   Future<void> setOutBoardingViewed() async {
     await _sharedPreferences.setBool(
@@ -16,23 +17,48 @@ class AppSettingSharedPreferences{
     );
   }
 
-   bool getOutBoardingViewed() {
-     return _sharedPreferences
-         .getBool(
-       ConstantsPrefsKeys.outBoardingViewedKey,
-     )
-         .onNull();
-   }
+  bool getOutBoardingViewed() {
+    return _sharedPreferences
+        .getBool(
+      ConstantsPrefsKeys.outBoardingViewedKey,
+    )
+        .onNull();
+  }
+
   Future<void> setToken(String token) async {
-    await _sharedPreferences.setString(Constants.tokenKey, token);
+    await setLoggedIn();
+    await _sharedPreferences.setString(ConstantsPrefsKeys.token, token);
   }
+
   String getToken() {
-    return _sharedPreferences.getString(Constants.tokenKey).onNull();
+    return _sharedPreferences.getString(ConstantsPrefsKeys.token).onNull();
   }
 
+  Future<void> setEmail(String email) async {
+    await _sharedPreferences.setString(ConstantsPrefsKeys.email, email);
+  }
 
+  String getEmail() {
+    return _sharedPreferences.getString(ConstantsPrefsKeys.email).onNull();
+  }
 
-  void clear(){
+  Future<void> setPassword(String password) async {
+    await _sharedPreferences.setString(ConstantsPrefsKeys.password, password);
+  }
+
+  String getPassword() {
+    return _sharedPreferences.getString(ConstantsPrefsKeys.password).onNull();
+  }
+
+  Future<void> setLoggedIn() async {
+    await _sharedPreferences.setBool(ConstantsPrefsKeys.loggedIn, true);
+  }
+
+  bool loggedIn() {
+    return _sharedPreferences.getBool(ConstantsPrefsKeys.loggedIn).onNull();
+  }
+
+  void clear() {
     _sharedPreferences.clear();
   }
 }
