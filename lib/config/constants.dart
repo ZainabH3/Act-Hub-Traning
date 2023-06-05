6147554ee60fc0d327412700a792badbcbb2d870
ZainabH3 <devzhsoftware@gmail.com>
2023-06-05 22:21:51 +0300

    edite constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,8 +2,8 @@ class Constants {
   static const String baseUrl = 'http://act-hub.actit.ps';
   static const double deviceWidth = 375;
   static const double deviceHeight = 812;
-  static const int  splashDuration = 3;
-  static const int  outBoardingDurationTime = 1;
+  static const int splashDuration = 3;
+  static const int outBoardingDurationTime = 1;
   static const double elevationAppBar = 0;
   static const double elevationButton = 0;
   static const int sliderItems = 3;
@@ -12,28 +12,35 @@ class Constants {
   static const String tokenKey = 'tokenKey';
   static const String outBoardingViewedKey = 'out_boarding_viewed';
   static const double getStateWidgetRenderStateElevation = 3;
-
+  static const int loginTimer = 1;
 }
+
 class ConstantsPrefsKeys {
   static const String outBoardingViewedKey = 'out_boarding_viewed';
   static const String token = 'token';
- // static const String loginFirstPartFlex = 'login_FirstPartFlex';
+  static const String email = 'email';
+  static const String password = 'password';
+  static const String loggedIn = 'logged_in';
 }
 
-
-class ApiConstants{
+class ApiConstants {
   static const String baseUrl = 'http://act-hub.actit.ps';
   static const String authorization = 'authorization';
   static const int sendTimeOutDuration = 120;
   static const int receiveTimeOutDuration = 120;
-
   static const String email = 'email';
   static const String password = 'password';
+  static const String passwordConfirmation = 'password_confirmation';
+  static const String phone = 'phone';
   static const String message = 'message';
-  static const String error = 'error';
   static const String errors = 'errors';
+  static const String error = 'error';
   static const String token = 'token';
   static const String badRequest = 'Bad Request';
-  static const String noInternetConnection = 'noInternetConnection';
+  static const String noInternetConnection = 'No Internet connection';
+  static const String acceptLanguage = 'Accept-Language';
+  static const String english = 'en';
+  static const String arabic = 'ar';
+  static const String status = 'status';
+  static const String name = 'name';
 }
-
