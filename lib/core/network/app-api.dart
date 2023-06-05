9a18245928801a62602956bfe60c822398e50f0e
ZainabH3 <devzhsoftware@gmail.com>
2023-06-05 22:20:13 +0300

    edite app api

--- lib/core/network/app-api.dart
+++ lib/core/network/app-api.dart
@@ -6,6 +6,7 @@ import 'package:retrofit/http.dart';
 import '../../config/constants.dart';
 import '../../config/request-constants.dart';
 import '../../features/auth/data/response/login-response.dart';
+import '../../features/auth/data/response/register_response.dart';
 part 'app-api.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
@@ -19,6 +20,15 @@ abstract class AppApi{
   @POST(RequestConstants.login)
   Future<LoginResponse> login(
       @Field(ApiConstants.email) email, @Field(ApiConstants.password) password);
+
+  @POST(RequestConstants.register)
+  Future<RegisterResponse> register(
+      @Field(ApiConstants.name) name,
+      @Field(ApiConstants.email) email,
+      @Field(ApiConstants.password) password,
+      @Field(ApiConstants.passwordConfirmation) passwordConfirmation,
+      @Field(ApiConstants.phone) phone,
+      );
 }
 
 
